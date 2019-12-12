7f15a426332605586a114b8e56974d4ef2b92d6c
mohamed792 <mohamedalsaid730@gmail.com>
2019-12-12 09:48:58 +0200

    fixing some issues with fetch api and gui

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,6 @@
 let mesg1 = document.querySelector('.mesg1');
 let mesg3 = document.querySelector('.mesg3');
+let errmesg = document.querySelector('.errmesg');
 let weatherForm = document.querySelector('form');
 
 
@@ -9,23 +10,28 @@ weatherForm.addEventListener('submit',(e)=>{
     let address = document.querySelector('.address').value;
     mesg1.style.display = 'block';
     mesg3.textContent  = "";
+    errmesg.textContent  = "";
 
     fetch(`/weather?address=${address}`).then((respons)=>{
-        if (respons.status !==200 || respons.error){
-            mesg1.textContent = respons.error;
+        if (respons.status !==200){
+            mesg1.textContent ="Sorry ,Please Try Again";
             return;
         }
         respons.json().then((data)=>{
             mesg1.style.display = 'none';
+            if(data.error){
+                errmesg.style.display = 'block';
+                errmesg.textContent  = data.error;
+               return;
+            }
             mesg3.style.display = 'block';
-
             for (item in data){
              mesg3.innerHTML +=`<tr> <td>${item}</td> <td>${ data[item]}</td> </tr>`;
            }
 
         });
     }).catch((err)=>{
-        console.log(err);
+        mesg1.textContent = "Sorry ,Please Try Again";
     });
 
 
